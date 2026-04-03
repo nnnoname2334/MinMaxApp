010c16a65a1ad375b49c376538d6fddc72253e7a
234989 <tregor2019@gmail.com>
2026-04-03 15:34:11 +0300

    xaml-комментарии

--- MinMaxApp/MathLogic.cs
+++ MinMaxApp/MathLogic.cs
@@ -2,9 +2,19 @@
 
 namespace MinMaxApp
 {
+    /// <summary>
+    /// Класс, содержащий математическую логику приложения.
+    /// </summary>
     public class MathLogic
     {
-        // Метод для нахождения нужного значения
+        /// <summary>
+        /// Метод для нахождения нужного значения (максимума или минимума).
+        /// </summary>
+        /// <param name="a">Первое вещественное число.</param>
+        /// <param name="b">Второе вещественное число.</param>
+        /// <param name="c">Третье вещественное число.</param>
+        /// <param name="isFindMax">Флаг: true для поиска максимума, false для минимума.</param>
+        /// <returns>Возвращает найденное экстремальное значение.</returns>
         public double Calculate(double a, double b, double c, bool isFindMax)
         {
             if (isFindMax)
